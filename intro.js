0ca9cf65f1119526f5fd3ead729a9d831f1cdcaa
Holyfield-beep <motteyholyfieldsenyo5@gmail.com>
2026-05-16 16:17:43 +0000

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,3 +1,11 @@
-   Name: Holyfield Senyo Mottey
-   Age: 26
-   Favorite Programming Language: Java
+// intro.js
+
+// My Personal Details
+const name = "Holyfield Senyo Mottey";
+const age = 26;
+const favoriteLanguage = "Java";
+
+// Logging the details to the console
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorite Programming Language: " + favoriteLanguage);
